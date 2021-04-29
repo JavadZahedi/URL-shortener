1df4ae45233d3d5d7ee7f013ca00b9b01fbf95cf
Javad <javad.zahedi2001@gmail.com>
2021-04-29 11:45:34 +0430

    before handle add url

--- URLshortener/urls.py
+++ URLshortener/urls.py
@@ -11,10 +11,4 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('sign-out/', views.sign_out, name='sign-out'),
     path('add-url/', views.add_url, name='add-url'),
-    # path('shortened-url/<slug:slug>', views.home, name='shortened-url'),
-    # path('sign-up/', views.home, name='sign-up'),
-    # path('sign-in/', views.home, name='sign-in'),
-    # path('dashboard/', views.home, name='dashboard'),
-    # path('sign-out/', views.home, name='sign-out'),
-    # path('add-url/', views.home, name='add-url'),
 ]
